4d89f7d5e8e56c6efba7dbf35b2e70b8a3abff94
Fabrizio Bautista <130857074+fabrixbau@users.noreply.github.com>
2024-04-15 12:46:36 -0600

    Clase de redondeos y decimales

---
+++ modulo4/clase2.py
@@ -0,0 +1,34 @@
+import decimal
+
+
+num1 = decimal.Decimal('10.345')
+num2 = decimal.Decimal('5.876')
+
+suma = num1 + num2
+print("Suma : ", suma)
+
+resta = num1 - num2
+print("Resta : ", resta)
+
+division = num1 / num2
+print("Divison : ", division)
+
+multiplicacion = num1 * num2
+print("Multiplicacion : ", multiplicacion)
+
+redondeo_2 = division.quantize(decimal.Decimal('0.00'))
+print(redondeo_2)
+
+redondeo_alza = (
+    division.quantize(decimal.Decimal('1.00'), rounding=decimal.ROUND_CEILING)
+)
+redondeo_baja = (
+    division.quantize(decimal.Decimal('1.00'), rounding=decimal.ROUND_FLOOR)
+)
+redondeo_cercano = (
+    division.quantize(decimal.Decimal("1.00"),
+                      rounding=decimal.ROUND_HALF_EVEN)
+)
+print("Redondeo hacia arriba : ", redondeo_alza)
+print("Redondeo hacia abajo : ", redondeo_baja)
+print("Redondeo cercano : ", redondeo_cercano)
