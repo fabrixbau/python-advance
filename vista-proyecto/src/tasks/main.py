0d1305360f0f70c255b1a637280e196ba1397977
Fabrizio Bautista <130857074+fabrixbau@users.noreply.github.com>
2024-04-16 15:55:28 -0600

    empezar a ver como gestionar un proyecto real

---
+++ vista-proyecto/src/tasks/main.py
@@ -0,0 +1,13 @@
+import logging
+from prefect import flow
+
+log = logging.getLogger()
+
+
+@flow(name="LINKEDIN SCRAPPER")
+def main_flow():
+    log.info("PROCESO DE EXTRACCIÓN")
+
+
+if __name__ == "__main__":
+    main_flow()
